63f9e4fbe26944554c00256c74d211e95515877d
Matthew Hefferon <matthefferon@gmail.com>
2022-11-06 09:12:38 -0700

    Added some links

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,18 +9,24 @@ const Hero = () => {
         <p className="text-sm p-2">
           {" "}
           Order Online for{" "}
-          <span className="underline underline-offset-4 hover:cursor-pointer">
-            Touchles Delivery
-          </span>
+          <a href="https://www.tesla.com/support/taking-delivery?redirect=no">
+            <span className="underline underline-offset-4 hover:cursor-pointer">
+              Touchles Delivery
+            </span>
+          </a>
         </p>
       </div>
       <div className="flex flex-col items-center justify-center text-sm lg:flex-row absolute inset-x-0 bottom-[10%]">
-        <button className="rounded-md bg-[#393c41] w-96 lg:w-64 mx-0 h-10 md:mx-4 mt-2 text-white">
-          Custom Order
-        </button>
-        <button className="rounded-md bg-[#f4f4f4] w-96 lg:w-64 mx-0 h-10 md:mx-4 mt-2">
-          Existing Inventory
-        </button>
+        <a href="https://www.tesla.com/model3/design">
+          <button className="rounded-md bg-[#393c41] w-96 lg:w-64 mx-0 h-10 md:mx-4 mt-2 text-white">
+            Custom Order
+          </button>
+        </a>
+        <a href="https://www.tesla.com/model3/design">
+          <button className="rounded-md bg-[#f4f4f4] w-96 lg:w-64 mx-0 h-10 md:mx-4 mt-2">
+            Existing Inventory
+          </button>
+        </a>
       </div>
       <div className="flex justify-center animate-bounce absolute inset-x-0 bottom-[3%]">
         <FiChevronDown size={24} />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,30 +4,32 @@ const Navbar = () => {
   return (
     <div className="flex justify-between items-center font-bold px-12 p-4 text-sm">
       <div>
+        <a href="https://tesla.com">
         <img
           className="h-3"
           src="data:image/svg+xml;base64,PHN2ZyBjbGFzcz0idGRzLWljb24gdGRzLWljb24tbG9nby13b3JkbWFyayB0ZHMtc2l0ZS1sb2dvLWljb24iIHZpZXdCb3g9IjAgMCAzNDIgMzUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHBhdGggZD0iTTAgLjFhOS43IDkuNyAwIDAgMCA3IDdoMTFsLjUuMXYyNy42aDYuOFY3LjNMMjYgN2gxMWE5LjggOS44IDAgMCAwIDctN0gwem0yMzguNiAwaC02Ljh2MzQuOEgyNjNhOS43IDkuNyAwIDAgMCA2LTYuOGgtMzAuM1Ywem0tNTIuMyA2LjhjMy42LTEgNi42LTMuOCA3LjQtNi45bC0zOC4xLjF2MjAuNmgzMS4xdjcuMmgtMjQuNGExMy42IDEzLjYgMCAwIDAtOC43IDdoMzkuOXYtMjFoLTMxLjJ2LTdoMjR6bTExNi4yIDI4aDYuN3YtMTRoMjQuNnYxNGg2Ljd2LTIxaC0zOHpNODUuMyA3aDI2YTkuNiA5LjYgMCAwIDAgNy4xLTdINzguM2E5LjYgOS42IDAgMCAwIDcgN3ptMCAxMy44aDI2YTkuNiA5LjYgMCAwIDAgNy4xLTdINzguM2E5LjYgOS42IDAgMCAwIDcgN3ptMCAxNC4xaDI2YTkuNiA5LjYgMCAwIDAgNy4xLTdINzguM2E5LjYgOS42IDAgMCAwIDcgN3pNMzA4LjUgN2gyNmE5LjYgOS42IDAgMCAwIDctN2gtNDBhOS42IDkuNiAwIDAgMCA3IDd6IiBmaWxsPSJjdXJyZW50Q29sb3IiLz48L3N2Zz4="
           alt="Tesla logo"
         />
+        </a>
       </div>
       <div className="hidden xl:inline">
         <ul className="flex justify-center hover:cursor-pointer">
-          <li className="py-1 px-3 hover:rounded hover:bg-black/5">Model S</li>
-          <li className="py-1 px-3 hover:rounded hover:bg-black/5">Model 3</li>
-          <li className="py-1 px-3 hover:rounded hover:bg-black/5">Model X</li>
-          <li className="py-1 px-3 hover:rounded hover:bg-black/5">Model Y</li>
-          <li className="py-1 px-3 hover:rounded hover:bg-black/5">
+          <li className="py-1 px-3 hover:rounded hover:bg-black/5"><a href="https://tesla.com/models">Model S</a></li>
+          <li className="py-1 px-3 hover:rounded hover:bg-black/5"><a href="https://tesla.com/model3">Model 3</a></li>
+          <li className="py-1 px-3 hover:rounded hover:bg-black/5"><a href="https://tesla.com/modelx">Model X</a></li>
+          <li className="py-1 px-3 hover:rounded hover:bg-black/5"><a href="https://tesla.com/modely">Model Y</a></li>
+          <li className="py-1 px-3 hover:rounded hover:bg-black/5"><a href="https://tesla.com/solarroof">
             Solar Roof
-          </li>
-          <li className="py-1 px-3 hover:rounded hover:bg-black/5">
+            </a></li>
+          <li className="py-1 px-3 hover:rounded hover:bg-black/5"><a href="https://tesla.com/solarpanels">
             Solar Panels
-          </li>
+            </a></li>
         </ul>
       </div>
       <div className="hidden xl:inline">
         <ul className="flex justify-center hover:cursor-pointer">
-          <li className="py-1 px-3 hover:rounded hover:bg-black/5">Shop</li>
-          <li className="py-1 px-3 hover:rounded hover:bg-black/5">Account</li>
+          <li className="py-1 px-3 hover:rounded hover:bg-black/5"><a href="https://shop.tesla.com">Shop</a></li>
+          <li className="py-1 px-3 hover:rounded hover:bg-black/5"><a href="https://tesla.com/teslaaccount">Account</a></li>
           <li className="py-1 px-3 hover:rounded hover:bg-black/5">Menu</li>
         </ul>
       </div>
